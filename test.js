f053909125dd42bb6de01d763f3af7b56b1bc8df
Aminul Islam <aminulislam565688@gmail.com>
2021-01-03 17:27:59 +0600

    This is My First Repo

---
+++ test.js
@@ -0,0 +1,11 @@
+var a =5 ;
+
+   switch (a){
+   case (a < 5):console.log("case 1 is right");
+   break;
+   case (a < 6):console.log("case 2 is right");
+   break;
+   case (a < 7):console.log("case 3 is right");
+   break
+   default:console.log("nothing is right");
+ }
