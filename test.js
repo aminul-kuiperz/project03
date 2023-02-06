b994fe08bbb5d5d4fd7b63c8ec76e48ea11312e9
x-aminulislam-x <aminulislam1developer@gmail.com>
2023-02-07 00:46:43 +0600

    i added a new variable

--- test.js
+++ test.js
@@ -1,11 +1,15 @@
-var a =5 ;
+var a = 5;
 
-   switch (a){
-   case (a < 5):console.log("case 1 is right");
-   break;
-   case (a < 6):console.log("case 2 is right");
-   break;
-   case (a < 7):console.log("case 3 is right");
-   break
-   default:console.log("nothing is right");
- }
+switch (a) {
+  case (a < 5): console.log("case 1 is right");
+    break;
+  case (a < 6): console.log("case 2 is right");
+    break;
+  case (a < 7): console.log("case 3 is right");
+    break
+  default: console.log("nothing is right");
+}
+
+const b = 9;
+
+console.log(b);
